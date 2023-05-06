74124bd5d5ed634fd80d3b17303ae99bdb1ba800
Anchen <li.anchen.au@gmail.com>
2023-05-07 01:15:08 +1000

    chore: update the api for my specific use case

--- extensions/api/blocking_api.py
+++ extensions/api/blocking_api.py
@@ -1,4 +1,5 @@
 import json
+import ssl
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from threading import Thread
 
@@ -8,16 +9,102 @@ from modules.text_generation import encode, generate_reply
 from extensions.api.util import build_parameters, try_start_cloudflared
 
 
+class ModelHandler:
+    @staticmethod
+    def handle_request(handler):
+        handler.send_response(200)
+        handler.end_headers()
+        response = json.dumps({'result': shared.model_name})
+        handler.wfile.write(response.encode('utf-8'))
+
+
+class GenerateHandler:
+    @staticmethod
+    def default_parameters():
+        return {
+            'max_new_tokens': 1000,
+            'do_sample': False,
+            'temperature': 0.1,
+            'top_p': 0.1,
+            'typical_p': 1,
+            'repetition_penalty': 1.18,
+            'top_k': 40,
+            'min_length': 0,
+            'no_repeat_ngram_size': 0,
+            'num_beams': 1,
+            'penalty_alpha': 0,
+            'length_penalty': 1,
+            'early_stopping': False,
+            'seed': -1,
+            'add_bos_token': True,
+            'truncation_length': 2048,
+            'ban_eos_token': False,
+            'skip_special_tokens': True,
+            'encoder_repetition_penalty':1,
+            'custom_stopping_strings': '',  
+            'stopping_strings': ["Assistant:"]
+        }
+
+    @staticmethod
+    def extract_response(response_text, prompt):
+            # Remove the prompt from the response_text
+            response_text = response_text.replace(prompt, '')
+
+            # Now find and remove the start of 'Output:'
+            if response_text.startswith('Output:'):
+                response_text = response_text[len('Output:'):].strip()
+
+            # And finally remove trailing '### Assistant'
+            if response_text.endswith('### Assistant'):
+                response_text = response_text[:response_text.rfind('### Assistant')].strip()
+
+            return response_text
+
+    @staticmethod
+    def handle_request(handler, body):
+        handler.send_response(200)
+        handler.send_header('Content-Type', 'application/json')
+        handler.end_headers()
+
+        prompt = body['prompt']
+        generate_params = GenerateHandler.default_parameters()
+        stopping_strings = generate_params.pop('stopping_strings')
+
+        generator = generate_reply(
+            prompt, generate_params, stopping_strings=stopping_strings)
+
+        answer = ''
+        for a in generator:
+            if isinstance(a, str):
+                answer = a
+            else:
+                answer = a[0]
+
+        extracted_text = GenerateHandler.extract_response(answer, prompt)
+        response = json.dumps({'results': [{'text': extracted_text}]})
+        handler.wfile.write(response.encode('utf-8'))
+
+
+class TokenCountHandler:
+    @staticmethod
+    def handle_request(handler, body):
+        handler.send_response(200)
+        handler.send_header('Content-Type', 'application/json')
+        handler.end_headers()
+
+        tokens = encode(body['prompt'])[0]
+        response = json.dumps({
+            'results': [{
+                'tokens': len(tokens)
+            }]
+        })
+        handler.wfile.write(response.encode('utf-8'))
+
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/api/v1/model':
-            self.send_response(200)
-            self.end_headers()
-            response = json.dumps({
-                'result': shared.model_name
-            })
-
-            self.wfile.write(response.encode('utf-8'))
+            ModelHandler.handle_request(self)
         else:
             self.send_error(404)
 
@@ -26,50 +113,20 @@ class Handler(BaseHTTPRequestHandler):
         body = json.loads(self.rfile.read(content_length).decode('utf-8'))
 
         if self.path == '/api/v1/generate':
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
-
-            prompt = body['prompt']
-            generate_params = build_parameters(body)
-            stopping_strings = generate_params.pop('stopping_strings')
-
-            generator = generate_reply(
-                prompt, generate_params, stopping_strings=stopping_strings)
-
-            answer = ''
-            for a in generator:
-                if isinstance(a, str):
-                    answer = a
-                else:
-                    answer = a[0]
-
-            response = json.dumps({
-                'results': [{
-                    'text': answer if shared.is_chat() else answer[len(prompt):]
-                }]
-            })
-            self.wfile.write(response.encode('utf-8'))
+            GenerateHandler.handle_request(self, body)
         elif self.path == '/api/v1/token-count':
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
-
-            tokens = encode(body['prompt'])[0]
-            response = json.dumps({
-                'results': [{
-                    'tokens': len(tokens)
-                }]
-            })
-            self.wfile.write(response.encode('utf-8'))
+            TokenCountHandler.handle_request(self, body)
         else:
             self.send_error(404)
 
 
 def _run_server(port: int, share: bool=False):
     address = '0.0.0.0' if shared.args.listen else '127.0.0.1'
-
     server = ThreadingHTTPServer((address, port), Handler)
+    sslctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    sslctx.check_hostname = False
+    sslctx.load_cert_chain(certfile='cert.pem', keyfile="key.pem")
+    server.socket = sslctx.wrap_socket(server.socket, server_side=True)
 
     def on_start(public_url: str):
         print(f'Starting non-streaming server at public url {public_url}/api')
@@ -80,11 +137,10 @@ def _run_server(port: int, share: bool=False):
         except Exception:
             pass
     else:
-        print(
-            f'Starting API at http://{address}:{port}/api')
+        print(f'Starting API at http://{address}:{port}/api')
 
     server.serve_forever()
 
 
 def start_server(port: int, share: bool = False):
-    Thread(target=_run_server, args=[port, share], daemon=True).start()
+    Thread(target=_run_server, args = [port, share], daemon=True).start()
